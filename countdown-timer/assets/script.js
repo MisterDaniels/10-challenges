a1a574bb3eb530dfbb7c585de426021af0315c2f
Daniel Silva <dodosilva51@gmail.com>
2020-09-12 01:14:39 -0300

    Added dinamic/logic of the countdown, some styles and mobile compability

---
+++ countdown-timer/assets/script.js
@@ -0,0 +1,36 @@
+const data = {
+    celebrations: [
+        {
+            name: 'Halloween',
+            date: new Date(new Date().getFullYear(), '10', '30'),
+            image: 'https://images.pexels.com/photos/632044/pexels-photo-632044.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
+        }
+    ]
+};
+
+var cardElement = document.getElementById('celebration');
+var nameElement = document.getElementById('name');
+var dayElement = document.getElementById('day');
+var hourElement = document.getElementById('hour');
+var minuteElement = document.getElementById('minute');
+var secondElement = document.getElementById('second');
+
+setInterval(() => {
+    data.celebrations.forEach(celebration => {
+        nameElement.innerHTML = celebration.name;
+        cardElement.style.backgroundImage = celebration.image;
+
+        const currentDate = new Date();
+        const diffMs = Math.abs(celebration.date - currentDate);
+
+        const diffSeconds = Math.floor(diffMs / 1000);
+        const diffMinutes = Math.floor(diffSeconds / 60);
+        const diffHours = Math.floor(diffMinutes / 60);
+        const diffdays = Math.floor(diffHours / 24);
+
+        secondElement.innerHTML = diffSeconds % 60;
+        minuteElement.innerHTML = diffMinutes % 60;
+        hourElement.innerHTML = diffHours % 24;
+        dayElement.innerHTML = diffdays;
+    });
+}, 1000)
